a8a09f586043dda55dd2587a5c02ddf983e4e126
FrankDunlop <frank.dunlop@gmail.com>
2022-05-08 22:34:04 +0100

    refactor for functional component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,56 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import SearchResults from './SearchResults';
 import * as BooksAPI from '../API/BooksAPI'
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
 
-class Search extends Component {
+function Search(props){
 
-    state = {
-        searchResults: []
+    const [searchResults, setsearchResults] = useState([])
+
+    const onShelfChange = (book, shelf) => {
+        props.onShelfChange(book, shelf)
     }
 
-    onShelfChange = (book, shelf) => {
-        this.props.onShelfChange(book, shelf)
-    }
-
-    search = (query) => {
-        this.setState(() => ({
-            searchResults: []
-        }))
+    const search = (query) => {
+        setsearchResults([])
 
         if(query !== '')
         {
             BooksAPI.search(query)
-                .then((searchResults) => {
-                    if(!searchResults.error)
+                .then((results) => {
+                    if(!results.error)
                     {
-                        this.setState(() => ({searchResults}))
+                        setsearchResults(results)
                     }
                 }
             )
         }
     }
 
-    render() {
-        const { bookList } = this.props;
+    const { bookList } = props;
 
-        return(
+    return(
         <div className="search-books">
             <div className="search-books-bar">
                 <Link to="/"><button className="close-search" type="button">Close</button></Link>
                 <div className="search-books-input-wrapper">
-                    <input type="text" placeholder="Search by title or author" onChange={(e) => this.search(e.target.value)}/>
+                    <input type="text" placeholder="Search by title or author" onChange={(e) => search(e.target.value)}/>
                 </div>
             </div>
             <div className="search-books-results">
                 <ol className="books-grid">
-                    <SearchResults bookList={bookList} searchResults={this.state.searchResults} onShelfChange={this.onShelfChange}/>
+                    <SearchResults bookList={bookList} searchResults={searchResults} onShelfChange={onShelfChange}/>
                 </ol>
             </div>
         </div>
-        )
-    }
+    )
 }
 
 SearchResults.propTypes = {
